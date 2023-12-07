d1f5e96ee4816e0c83f85dd47a87d4fb720166d0
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2023-12-07 21:30:30 +0000

    Update Rasberry Pi robot sensor follow the line code 1.py
    New code, for three sensors

--- Rasberry Pi robot sensor follow the line code 1.py
+++ Rasberry Pi robot sensor follow the line code 1.py
@@ -1,46 +1,54 @@
-pip install RPi.GPIO
 import RPi.GPIO as GPIO
 import time
 
 
+Left_sensor_pin =      #modify for use!
+middle_sensor_pin = 
+right_senosr_pin =
+left_motor_pin =
+right_motor_pin  =
 
-GPIO.setmode(GPIO.BOARD)
-left_motor = [,]  #customise
-right_motor = [,]  #customise
-GPIO.setup(left_motor, GPIO.OUT)
-GPIO.setup(right_motor, GPIO.OUT)
-left_sensors = [,]  #customise
-right_sensors = [,] #customise
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(left_sensor_pin, GPIO.IN)
+GPIO.setup(middle_sensor_pin, GPIO.IN)
+GPIO.setup(right_sensor_pin, GPIO.IN)
+GPIO.setup(left_motor_pin, GPIO.OUT)
+GPIO.setup(right_motor_pin, GPIO.OUT)
 
-GPIO.setup(left_sensors, GPIO.IN)
-GPIO.setup(right_sensors, GPIO.IN)
-def forward():
-    GPIO.output(left_motor[0], GPIO.HIGH)
-    GPIO.output(left_motor[1], GPIO.LOW)
-    GPIO.output(right_motor[0], GPIO.HIGH)
-    GPIO.output(right_motor[1], GPIO.LOW)
+left_motor = GPIO.PWM(left_motor_pin, 100)
+right_motor = GPIO.PWM(right_motor_pin, 100)
+left_motor.start(0)
+right_motor.start(0)
+
+def forward(left_speed, right_speed):
+    left_motor.ChangeDutyCycle(left_speed)
+    right_motor.ChangeDutyCycle(right_speed)
+    GPIO.output(left_motor_pin, GPIO.HIGH)
+    GPIO.output(right_motor_pin, GPIO.HIGH)
 def left():
-    GPIO.output(left_motor[0], GPIO.LOW)
-    GPIO.output(left_motor[1], GPIO.HIGH)
-    GPIO.output(right_motor[0], GPIO.HIGH)
-    GPIO.output(right_motor[1], GPIO.LOW)
+    forward(100,70) #ADJUST!!!
 def right():
-    GPIO.output(left_motor[0], GPIO.HIGH)
-    GPIO.output(left_motor[1], GPIO.LOW)
-    GPIO.output(right_motor[0], GPIO.LOW)
-    GPIO.output(right_motor[1], GPIO.HIGH)
-try:
+    forwards(70,100) #ADJUST!!!
+def stop():
+    left_motor.ChangeDutyCycle(0)
+    right_motor.ChangeDutyCycle(0)
+    GPIO.output(left_motor_pin, GPIO.LOW)
+    GPIO.output(right_motor_pin, GPIO.LOW)
+def Lava_Palava():
     while True:
-        left_sensor_values = [GPIO.input(pin) for pin in left_sensors]
-        right_sensor_values = [GPIO.input(pin) for pin in right_sensors]
-        if all(value == GPIO.LOW for value in left_sensor_values) and all(value == GPIO.LOW for value in right_sensor_values):
-            forward()
-        elif any(value == GPIO.HIGH for value in left_sensor_values) and all(value == GPIO.LOW for value in right_sensor_values):
-            right()
-        elif all(value == GPIO.LOW for value in left_sensor_values) and any(value == GPIO.HIGH for value in right_sensor_values):
-            left()
+        left_sensor = GPIO.input(left_sensor_pin)
+        middle_sensor = GPIO.input(middle_sensor_pin)
+        right_sensor = GPIO.input(right_sensor_pin)
+        
+        if middle_sensor == 0:
+            if left_sensor == 1:
+                right()
+            elif right_sensor == 1:
+                left()
+            else:
+                stop()
         else:
-            #This coould mean both sensors on or off the line so I will leave this until disccussion
-            pass
-except KeyboardInterrupt:
-    GPIO.cleanup()
+            forward(100,100)
+        time.sleep(0.1) #CUSTOMISE BUT I THINK 0.1 SECONDS PER CHECK IS GOOD ENOUGH
+
+Lava_Palava()
