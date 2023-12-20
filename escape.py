d0674b339f4438bbbd2d2f117663f64f825e668b
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2023-12-20 18:36:12 +0000

    Update escape.py
    Guys this is not yet complete

--- escape.py
+++ escape.py
@@ -1,34 +1,93 @@
-import RPi.GPIO as GPIO 
-from gpiozero import Motor 
-import time 
+from gpiozero import Motor, DistanceSensor #Unlike Rpi.GPIO, gpiozero always uses BCM. This cannot be configured unfortunately so just keep in mind when assigning GPIO pins
+import time
 
-left_motor = Motor(forward = ?, backward = ?) #customise 
-right_motor = Motor(forward = ?, backward = ?) #customise 
-left_sensor_pin = #put number for GPIO pin 
-right_sensor_pin = #same as line 10 
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(left_sensor_pin, GPIO.IN) 
-GPIO.setup(right_sensor_pin, GPIO.IN) 
 
-def obstacles_left(): 
-    return GPIO.input(left_sensor_pin) == GPIO.HIGH 
-def obstacles_right(): 
-    return GPIO.input(right_sensor_pin) == GPIO.HIGH 
+full_speed_ahead = 1
+small_turn = 0.9
+medium turn = 0.8
+big_turn = 0.5
+emergency_turn = 0.3
 
-while True: 
-    if not obstacle_left() and not obstacle_right(): 
-        left_motor.forward() 
-        right_motor.forward() 
-    elif not obstacle_left() and obstacle_right(): 
-        left_motor.backward() 
-        right_motor.forward() 
-    elif obstacle_left() and not obstacle_right(): 
-        left_motor.forward() 
-        right_motor.backward() 
-    else: 
-        left_motor.forward() 
-        right_motor.forward() 
-    time.sleep(0.1) #adjust! 
-left_motor.stop() 
-right_motor.stop() 
-GPIO.cleanup() 
+left_motor = Motor(forward = ,backward = )
+right_motor = Motor(forward = ,backward = )
+
+front_sensor = DistanceSensor(echo =, trigger =)
+left_sensor = DistanceSensor(echo =, trigger =)
+right_sensor = DistanceSensor(echo =, trigger =)
+back_sensor = DistanceSensor(echo =, trigger =)
+
+def forward():
+    left_motor.forward(full_speed_ahead)
+    right_motor.forward(full_speed_ahead)
+    
+def left_small():
+    left_motor.forward(small_turn)
+    right_motor.forward(full_speed_ahead)
+    
+def right_small():
+    left_motor.forward(full_speed_ahead)
+    right_motor.forward(small_turn)
+    
+def left_medium():
+    left_motor.forward(medium_turn)
+    right_motor.forward(full_speed_ahead)
+    
+def right_medium():
+    left_motor.forward(full_speed_ahead)
+    right_motor.forward(medium_turn)
+    
+def left_big():
+    left_motor.forward(big_turn)
+    right_motor.forward(full_speed_ahead)
+    
+def right_big():
+    left_motor.forward(full_speed_ahead)
+    right_motor.forward(big_turn)
+    
+def backward():
+    left_motor.backward(full_speed_ahead)
+    right_motor.backward(full_speed_ahead)
+    
+def backward_left_small():
+    left_motor.forward(small_turn)
+    right_motor.forward(full_speed_ahead)
+    
+def backward_right_small():
+    left_motor.forward(full_speed_ahead)
+    right_motor.forward(small_turn)
+    
+def backward_left_medium():
+    left_motor.forward(medium_turn)
+    right_motor.forward(full_speed_ahead)
+    
+def backward_right_medium():
+    left_motor.forward(full_speed_ahead)
+    right_motor.forward(medium_turn)
+    
+def backward_left_big():
+    left_motor.forward(big_turn)
+    right_motor.forward(full_speed_ahead)
+    
+def backward_right_big():
+    left_motor.forward(full_speed_ahead)
+    right_motor.forward(big_turn)
+    
+def backward_left_emergency():
+    left_motor.forward(emergency_turn)
+    right_motor.forward(full_speed_ahead)
+    
+def backward_right_emergency():
+    left_motor.forward(full_speed_ahead)
+    right_motor.forward(emergency_turn)
+    
+def stop():
+    left_motor.stop()
+    right_motor.stop()
+    
+while True:
+    forward_distance = forward_sensor.distance() #LOOK HERE I DON'T KNOW IF THERE IS A BRACKET HERE MAYBE YES MAYBE NO
+    left_distance = left_sensor.distance()
+    right_distance = right_senor.distance()
+    back_distance
+    
+
