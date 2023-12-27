c2d6a99b640067a81d3cbb4bd4f3973dfcf8b1ea
18lliu <152213070+18lliu@users.noreply.github.com>
2023-12-27 15:24:24 +0000

    Update lava.py
    So guys there is the new variable called N which, btw, DO NOT CHANGE OR MODIFY! Otherwise it is just me improving the code

--- lava.py
+++ lava.py
@@ -78,5 +78,5 @@ def Lava_Palava():
                             forward(100,100) #We are at the finish. Robot needs to cross finish line to count as a win so we still have to forwards a bit
                             time.sleep(0.01) #Modify if you guys want but I don't think we need to
                             sleep(0.0001) #At the start n is 0 because defined it. After we start doing circuits it detects if being placed on the staright line at the start it will run so be careful not to acccidently make it run or anything
-                break
+                    break
 Lava_Palava()
