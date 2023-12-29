dad3877e2950d976952cf3580e5f16fc282ead73
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2023-12-29 17:28:37 +0000

    Update lava.py

--- lava.py
+++ lava.py
@@ -6,32 +6,34 @@ import time
 left_sensor_pin =  29    #modify for use!
 middle_sensor_pin = 31#We can just glue three jumper wires together.
 right_sensor_pin = 33
-right_motor_input_1 = 21  #same as the ones for the remote control: this makes it less confusing. It also allows us to manually override the robot 
-right_motor_input_2 = 22 # if something doesnt work
-left_motor_input_3 = 23
-left_motor_input_4 = 24
+
+rightinput_1 = 21  #same as the ones for the remote control: this makes it less confusing. It also allows us to manually override the robot 
+rightinput_2 = 22
+leftinput_1 = 23
+leftinput_2 = 24
+GPIO.output(rightinput_1, HIGH)
+GPIO.output(rightinput_2, LOW)# if something doesnt work
+GPIO.output(leftinput_1, HIGH)
+GPIO.output(leftinput_1, LOW)
 
 n = 0 #DO NOT CHANGE!
 
-GPIO.setwarning(False)
+GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(left_sensor_pin, GPIO.IN)
 GPIO.setup(middle_sensor_pin, GPIO.IN)
 GPIO.setup(right_sensor_pin, GPIO.IN)
-GPIO.setup(left_motor_pin, GPIO.OUT)
-GPIO.setup(right_motor_pin, GPIO.OUT)
 
-left_motor = GPIO.PWM(left_motor_pin, 100)
-right_motor = GPIO.PWM(right_motor_pin, 100)
+
+left_motor = GPIO.PWM(leftinput_1, 100)
+right_motor = GPIO.PWM(rightinput_1, 100)
 left_motor.start(0)
 right_motor.start(0)
 
 def forward(left_speed, right_speed):
     left_motor.ChangeDutyCycle(left_speed)
     right_motor.ChangeDutyCycle(right_speed)
-    GPIO.output(left_motor_pin, GPIO.HIGH)
-    GPIO.output(right_motor_pin, GPIO.HIGH)
 def left():
     forward(70,100) #ADJUST!!!
 def right():
@@ -67,7 +69,7 @@ def Lava_Palava():
                     right()
                     time.sleep(0.0001)
                     if left_sensor and right_sensor == 0:
-                        ultimate_right():
+                        ultimate_right()
             else:
                 if middle_sensor == 1 and left_sensor and right_sensor == 0:
                     forward(100,100)
@@ -80,3 +82,5 @@ def Lava_Palava():
                         time.sleep(0.1) #Sleep a little bit more so our hands can get off the robot
                     break
 Lava_Palava()
+
+
