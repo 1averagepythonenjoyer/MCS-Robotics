3c7d38c195d9bfadb8e80d2da3d0a74dc962694f
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-01-03 20:48:49 +0000

    Update lava.py

--- lava.py
+++ lava.py
@@ -12,6 +12,7 @@ GPIO.setup(left_sensor_pin, GPIO.IN)
 GPIO.setup(middle_sensor_pin, GPIO.IN)
 GPIO.setup(right_sensor_pin, GPIO.IN)
 
+n = 0
 
 right_motor_input_1 = 21  #same as the ones for the remote control: this makes it less confusing. It also allows us to manually override the robot 
 right_motor_input_2 = 22
@@ -28,12 +29,10 @@ GPIO.output(right_motor_input_2, GPIO.LOW)# if something doesnt work
 GPIO.output(left_motor_input_1, GPIO.HIGH)
 GPIO.output(left_motor_input_1, GPIO.LOW)
 
-n = 0 #DO NOT CHANGE!
-
 left_motor = GPIO.PWM(left_motor_input_1, 100)
 right_motor = GPIO.PWM(right_motor_input_1, 100)
-left_motor.start(0)
 right_motor.start(0)
+left_motor.start(0)
 
 def forward(left_speed, right_speed):
     left_motor.ChangeDutyCycle(left_speed)
@@ -52,10 +51,15 @@ def ultimate_right():
     forward(100,20) #ADJUST!!!
 def Lava_Palava():
     while True:
+        global n
         left_sensor = GPIO.input(left_sensor_pin)
         middle_sensor = GPIO.input(middle_sensor_pin)
         right_sensor = GPIO.input(right_sensor_pin)
-        if left_sensor and middle_sensor and right_sensor == 1:
+        
+        if n >= 2:
+            n += 1
+        
+        if left_sensor and middle_sensor and right_sensor == 0:
             sleep(0.0001)
         elif left_sensor and middle_sensor == 1:
             left_a_bit()
@@ -80,12 +84,10 @@ def Lava_Palava():
                     forward(100,100)
                     time.sleep(0.0001)
                 else:
-                    if n != 0:
+                    if n < 1:
                         while left_sensor and middle_sensor and right_sensor == 0:
                             forward(100,100) #We are at the finish. Robot needs to cross finish line to count as a win so we still have to forwards a bit
                             time.sleep(0.01) #Modify if you guys want but I don't think we need to
                         time.sleep(0.1) #Sleep a little bit more so our hands can get off the robot
                     break
-                    
 Lava_Palava()
-
