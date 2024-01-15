5e43a0b48658133fec8861849d359cfc1d6aa328
18lliu <152213070+18lliu@users.noreply.github.com>
2024-01-15 14:06:01 +0000

    Update lava.py
    I decided to not use threading and asyncio since I don't know much about threading. I decided to just spam checking kill switch everywhere. I mean it is not like we need robot to stop the exact millisecond to the exact decimal point the order was given out. Maximum of 0.2 seconds delay is pretty good for us, since we don't need that much.

--- lava.py
+++ lava.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-import threading
 from approxeng.input.selectbinder import ControllerResource
 
 GPIO.setmode(BOARD)
@@ -41,8 +40,8 @@ GPIO.output(input1, GPIO.HIGH)
 input4 = 24
 GPIO.setup(input2, GPIO.OUT)
 GPIO.output(input2, GPIO.LOW)
-
-def kill_switch():
+    
+def check_kill_switch():
     while joystick.connected:
         joystick.check_presses()
         if joystick.presses.cross: #kill switch so cross seems good but change if you want to
@@ -51,6 +50,8 @@ def kill_switch():
             GPIO.output(input3, GPIO.LOW)
             GPIO.output(input4, GPIO.LOW)
             quit()
+        else:
+            return
 
 def Lava_Palava():
     while True:
@@ -70,44 +71,67 @@ def Lava_Palava():
         print("middle =", middle_sensor)
         print("right =", right_sensor)
         print("-------------------------------------------")
+
+        check_kill_switch()
         
         if left_sensor == 0 and middle_sensor == 0 and right_sensor == 0:
+            check_kill_switch()
             time.sleep(0.0001)
         elif left_sensor == 1 and middle_sensor == 1 and right_sensor == 0:
+            check_kill_switch()
             mix()
             time.sleep(0.0001)
         elif right_sensor ==1  and middle_sensor == 1 and left_sensor == 0:
+            check_kill_switch()
             mix()
             time.sleep(0.0001)
         else:
             if middle_sensor == 0:
                 if left_sensor == 1 and right_sensor == 0:
+                    check_kill_switch()
                     mix()
                     time.sleep(0.0001)
                     if left_sensor == 0 and right_sensor == 0:
+                        check_kill_switch()
                         mix()
                         time.sleep(0.0001)
                 elif right_sensor == 1:
+                    check_kill_switch()
                     mix()
                     time.sleep(0.0001)
                     if left_sensor == 0 and right_sensor == 0:
+                        check_kill_switch()
                         mix()
                         time.sleep(0.0001)
             else:
                 if middle_sensor == 1 and left_sensor == 0 and right_sensor == 0:
+                    check_kill_switch()
                     mix()
                     time.sleep(0.0001)
                 else:
                     if left_sensor == 0 and middle_sensor == 0 and right_sensor == 0:
+                        check_kill_switch()
                         mix()#We are at the finish. Robot needs to cross finish line to count as a win so we still have to forwards a bit
-                        time.sleep(1.2) #Modify if you guys want but I don't think we need to
-                            
-                        time.sleep(0.01) #Sleep a little bit more so our hands can get off the robot
+                        time.sleep(0.2)  #These add up to 1.2 seconds excluding sleeping extra 0.1 seconds. I did this so there would be as little delay for kill switch as possible. Maximum of 0.2 seconds delay is not too bad.
+                        check_kill_switch()
+                        time.sleep(0.2)
+                        check_kill_switch()
+                        time.sleep(0.2)
+                        check_kill_switch()
+                        time.sleep(0.2)
+                        check_kill_switch()
+                        time.sleep(0.2)
+                        check_kill_switch()
+                        time.sleep(0.2)
+                        check_kill_switch()
+                        time.sleep(0.1) #Sleep a little bit more so our hands can get off the robot
                         n+=1
                         break
+                    check_kill_switch()
                     break
                         
 if left_sensor == 0 and right_sensor == 0 and middle_sensor == 1:
+    check_kill_switch()
     Lava_Palava()
     
 
