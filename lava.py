8f4df0978a63ce681a2df5c8d519f07dc5494f9b
18lliu <152213070+18lliu@users.noreply.github.com>
2024-01-31 14:23:46 +0000

    Update lava.py

--- lava.py
+++ lava.py
@@ -11,19 +11,10 @@ middle_sensor_pin = 10
 right_sensor_pin = 12
 
 n = 0
-m = False
-presses = joystick.check_presses()
 
 GPIO.setup(left_sensor_pin, GPIO.IN)
 GPIO.setup(middle_sensor_pin, GPIO.IN)
 GPIO.setup(right_sensor_pin, GPIO.IN)
-
-def stop():
-    global m
-    global presses #VERY IMPORTNAT LOOK HERE!!!
-    if '' in presses: #stop button whatever it may be
-        m = True
-        if
 
 def Lava_Palava():
     while True:
@@ -43,64 +34,43 @@ def Lava_Palava():
         if left_sensor == 1 and middle_sensor == 1 and right_sensor == 0:
             mix(-0.95,1)
             time.sleep(0.0001)
-            stop()
         elif right_sensor == 1  and middle_sensor == 1 and left_sensor == 0:
             mix(0.95,1)
             time.sleep(0.0001)
-            stop()
         else:
             if middle_sensor == 0:
                 if left_sensor == 1 and right_sensor == 0:
                     mix(-0.8,1)
                     time.sleep(0.0001)
-                    stop()
                     if left_sensor == 0 and right_sensor == 0:
                         mix(-0.5,1)
                         time.sleep(0.0001)
-                        stop()
                 elif right_sensor == 1:
                     mix(0.8,1)
                     time.sleep(0.0001)
-                    stop()
                     if left_sensor == 0 and right_sensor == 0:
                         mix(0.5,1)
                         time.sleep(0.0001)
-                        stop()
             else:
                 if middle_sensor == 1 and left_sensor == 0 and right_sensor == 0:
                     mix(0,1)
                     time.sleep(0.0001)
-                    stop()
                 else:
                     if left_sensor == 0 and middle_sensor == 0 and right_sensor == 0:
                         mix(0,1)
                         time.sleep(0.2)
-                        stop()
                         time.sleep(0.2)
-                        stop()
                         time.sleep(0.2)
-                        stop()
                         time.sleep(0.2)
-                        stop()
                         time.sleep(0.2)
-                        stop()
                         time.sleep(0.2)
-                        stop()
                         n+=1
                         mix(0,0)
                         break
                     break
                         
 while n <= 3:
-    global presses
-    try:
-        with ControllerResource() as joystick:
-            while joystick.connected:
-                if 'L1 Trigger' in presses or n > 0: #start code button
-                    if left_sensor == 1 or right_sensor == 1 or middle_sensor == 1:
-                        time.sleep(0.1)
-                        stop()
-                        Lava_Palava()
-    except IOError:
-        #print('unable to find any joysticks')
-        #Don't know what to put here
+    if left_sensor == 1 or right_sensor == 1 or middle_sensor == 1:
+        time.sleep(0.1)
+        Lava_Palava()
+    
