1e010e44d131dbe29798c9acce6cc64990c90524
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-01 17:16:37 +0100

    Update manualzomb.py
    add servo pin defs

--- manualzomb.py
+++ manualzomb.py
@@ -1,13 +1,19 @@
 import time
 import RPi.GPIO as GPIO 
+from RPiMotorLib import RPiservo
 from approxeng.input.selectbinder import ControllerResource 
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 sol_ctrl = 10 #solenoid control pin. Pin 8, GPIO 8 (BOARD, BCM) ALways use BOARD
-
 GPIO.setup(sol_ctrl, GPIO.OUT)
+
+servo_1 =  #servo 3 pin
+servo_2 = #servo 2 pin 
+GPIO.setup(servo_1, GPIO.OUT)
+GPIO.setup(servo_2, GPIO.OUT)
+      
 
 while joystick.connected:
         # Call check_presses at the top of the loop to check for presses and releases, this returns the buttons
@@ -16,9 +22,11 @@ while joystick.connected:
         # value anywhere.
         joystick.check_presses()
         # Check for a button press
-        if joystick.presses.cross:
-            print('Fire request given waiting 1second...') #waiting is just for now: REMEMBER TO REMOVE BEFORE COMPETITION: DEAD TIME
+        if joystick.presses.l2:
             time.sleep(1)
             GPIO.output(sol_ctrl, GPIO.HIGH)
             time.sleep(0.1) #hopefully this is enough. may need to modify. 
             GPIO.output(sol_ctrl, GPIO.LOW)
+
+
+            
