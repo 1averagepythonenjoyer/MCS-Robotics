ddc354f8157097be7c0d2dd59fa50263e2482b22
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-09 17:58:56 +0100

    Update mcsmotors.py

--- mcsmotors.py
+++ mcsmotors.py
@@ -1,23 +1,19 @@
 from time import sleep
 import RPi.GPIO as GPIO # remember to install
 from approxeng.input.selectbinder import ControllerResource # remember to install
-from mcsmotors import *
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.cleanup()
-
 PWM_ENA = 13  
-PWM_ENB = 15
-
 GPIO.setup(PWM_ENA, GPIO.OUT)
-GPIO.setup(PWM_ENB, GPIO.OUT)
-
 PWMa = GPIO.PWM(PWM_ENA, 100)
-PWMb= GPIO.PWM(PWM_ENB, 100)
-
 PWMa.start(0)
+
+PWM_ENB = 15
+GPIO.setup(PWM_ENB, GPIO.OUT)
+PWMb= GPIO.PWM(PWM_ENB, 100)
 PWMb.start(0)
 
 #h bridge logic 
@@ -37,58 +33,35 @@ GPIO.output(IN4, GPIO.LOW)
 
 RMcorrection = 1.0 #to offset drift of motors (1 motor spinning faster than the other: left motor spins faster than the right I think)
 LMcorrection = 0.8
-LOWSPEED = 0.4
 
-def remotecontrol():
-    try:
-        while True:
-            try:
-                with ControllerResource as joystick:
-                    while joystick.connected:
-                        global lvalue
-                        global rvalue 
-                        global lmotor
-                        global rmotor
-                        global lowspeedheld
-                        lvalue = joystick['ly'] #joystick read values
-                        rvalue = joystick['ry']
-                        #check hbridge for left motor
-                        if lvalue < 0:     #backward
-                            GPIO.output(IN1, GPIO.LOW)
-                            GPIO.output(IN2, GPIO.HIGH)
-                        elif lvalue >= 0:    #forward/neutral
-                            GPIO.output(IN1, GPIO.HIGH)
-                            GPIO.output(IN2, GPIO.LOW)
-                        #check hbridge for right motor
-                        if rvalue < 0:  #backward
-                            GPIO.output(IN3, GPIO.LOW)
-                            GPIO.output(IN4, GPIO.HIGH)
-                        elif rvalue >= 0:    #forward/neutral
-                            GPIO.output(IN3, GPIO.HIGH)
-                            GPIO.output(IN4, GPIO.LOW)
+def map_range(a, input_min, input_max, output_min, output_max): #nicky's function. Not entirely sure what it does. 
+    return (a - input_min) * (output_max - output_min) // (input_max - input_min) + output_min #a is placeholder value
 
-                        lmotor = map_range(lvalue,0,100,0,100)   #maps joystick values to actual pwm values
-                        rmotor = map_range(rvalue,0,100,0,100)    #need to check deadzones
-
-                        lowspeedheld = joystick['r1']   #if r1 is not held, library returns value of None, so we need to check that it is not None
-                        if lowspeedheld is not None: 
-                            PWMa.ChangeDutyCycle(lmotor * LOWSPEED)
-                            PWMb.ChangeDutyCycle(rmotor * LOWSPEED)
-                        else:
-                            PWMa.ChangeDutyCycle(lmotor)
-                            PWMb.ChangeDutyCycle(rmotor)
-            except IOError:
-                print("No Controller found")
-                sleep(1)
-    except KeyboardInterrupt:
-        print("Keyboard interrupt detected: stopping")
-        exit()
-
-
-
-
-        
+def setmotor(left_power, right_power):
+    #check h bridge
+    if left_power < 0:
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.HIGH)
+    if left_power >= 0:
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+    if right_power < 0:
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.HIGH)
+    if right_power >= 0:
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+    global lmotor 
+    global rmotor
+    lmotor = map_range(left_power,0,100,0,100) #values to be sent to motors
+    rmotor = map_range(right_power,0,100,0,100) 
+    #send values to motor controller 
+    PWMa.ChangeDutyCycle(lmotor * LMcorrection)
+    PWMb.ChangeDutyCycle(rmotor * RMcorrection)
+    
+    
     
 
-
     
+    
+
