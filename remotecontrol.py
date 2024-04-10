b49dc3dea2954dbcab6cc73627fb95b50dea419f
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-10 17:29:08 +0100

    Update remotecontrol.py

--- remotecontrol.py
+++ remotecontrol.py
@@ -1,51 +1,10 @@
 from time import sleep
 import RPi.GPIO as GPIO # remember to install
 from approxeng.input.selectbinder import ControllerResource # remember to install
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.cleanup()
-
-PWM_ENA = 13  
-PWM_ENB = 15
-
-GPIO.setup(PWM_ENA, GPIO.OUT)
-GPIO.setup(PWM_ENB, GPIO.OUT)
-
-PWMa = GPIO.PWM(PWM_ENA, 100)
-PWMb= GPIO.PWM(PWM_ENB, 100)
-
-PWMa.start(0)
-PWMb.start(0)
-
-#h bridge logic 
-#currently set to forward motion
-IN1= 19      #INPUT1 and INPUT2 logic pins on L298 control direction of right motor 
-GPIO.setup(IN1, GPIO.OUT) 
-GPIO.output(IN1, GPIO.HIGH)
-IN2 = 21
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.output(IN2, GPIO.LOW)
-IN3 = 16      #INPUT3 and INPUT4 logic pins control left motor
-GPIO.setup(IN3, GPIO.OUT)
-GPIO.output(IN3, GPIO.HIGH)
-IN4 = 18
-GPIO.setup(IN4, GPIO.OUT)
-GPIO.output(IN4, GPIO.LOW)
-
-RMcorrection = 1.0 #to offset drift of motors (1 motor spinning faster than the other: left motor spins faster than the right I think)
-LMcorrection = 0.785
-LOWSPEED = 0.4
-global rvalue
-global lvalue 
-global rmotor
-global lmotor
-global lowspeedheld
-
+from mcsmotors import *
 while True:
     try:
-        with ControllerResource as joystick:
+        with ControllerResource() as joystick:
             while joystick.connected:
                     rvalue = joystick['ry']
                     lvalue = joystick['ly'] #joystick read values
