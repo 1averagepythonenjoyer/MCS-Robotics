1c2e036667d9b5ae2ff1d4c5e2f8cd74bf45b1af
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-10 16:43:05 +0100

    Update remotecontrol.py

--- remotecontrol.py
+++ remotecontrol.py
@@ -14,7 +14,7 @@ GPIO.setup(PWM_ENA, GPIO.OUT)
 GPIO.setup(PWM_ENB, GPIO.OUT)
 
 PWMa = GPIO.PWM(PWM_ENA, 100)
-PWMb = GPIO.PWM(PWM_ENB, 100)
+PWMb= GPIO.PWM(PWM_ENB, 100)
 
 PWMa.start(0)
 PWMb.start(0)
@@ -39,60 +39,48 @@ LMcorrection = 0.785
 LOWSPEED = 0.4
 
 def remotecontrol():
-    while True:
-        try:
-            with ControllerResource as joystick:
-                while joystick.connected:
-                    global rvalue
-                    global lvalue 
-                    global rmotor
-                    global lmotor
-                    global lowspeedheld
-                    rvalue = joystick['ry']
-                    lvalue = joystick['ly'] #joystick read values
-                    if rvalue < 0:  #backward
-                        GPIO.output(IN1, GPIO.LOW)
-                        GPIO.output(IN2, GPIO.HIGH)
-                    elif rvalue >= 0:    #forward/neutral
-                        GPIO.output(IN1, GPIO.HIGH)
-                        GPIO.output(IN2, GPIO.LOW)
-                    #check hbridge for left motor
-                    if lvalue < 0:     #backward
-                        GPIO.output(IN3, GPIO.LOW)
-                        GPIO.output(IN4, GPIO.HIGH)
-                    elif lvalue >= 0:    #forward/neutral
-                        GPIO.output(IN3, GPIO.HIGH)
-                        GPIO.output(IN4, GPIO.LOW)
-                    #check hbridge for right motor
-                    rmotor = map_range(rvalue,0,100,0,100)    #need to check deadzones
-                    lmotor = map_range(lvalue,0,100,0,100)   #maps joystick values to actual pwm values
-                    print('Left: {}, Right: {}'.format(rvalue, lvalue))
-                    print('Left duty: {}, Right duty: {}'.format(rmotor, lmotor))
-                    lowspeedheld = joystick['r1']   #if r1 is not held, library returns value of None, so we need to check that it is not None
+    global rvalue
+    global lvalue 
+    global rmotor
+    global lmotor
+    global lowspeedheld
+    rvalue = joystick['ry']
+    lvalue = joystick['ly'] #joystick read values
+    if rvalue < 0:  #backward
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.HIGH)
+    elif rvalue >= 0:    #forward/neutral
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+    #check hbridge for left motor
+    if lvalue < 0:     #backward
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.HIGH)
+    elif lvalue >= 0:    #forward/neutral
+        GPIO.output(IN3, GPIO.HIGH)
+        GPIO.output(IN4, GPIO.LOW)
+    #check hbridge for right motor
+    rmotor = map_range(rvalue,0,100,0,100)    #need to check deadzones
+    lmotor = map_range(lvalue,0,100,0,100)   #maps joystick values to actual pwm values
+    print('Left: {}, Right: {}'.format(rvalue, lvalue))
+    print('Left duty: {}, Right duty: {}'.format(rmotor, lmotor))
+    lowspeedheld = joystick['r1']   #if r1 is not held, library returns value of None, so we need to check that it is not None
                     
-                    if lowspeedheld is not None: 
-                        PWMa.ChangeDutyCycle(lmotor * LOWSPEED)
-                        PWMb.ChangeDutyCycle(rmotor * LOWSPEED)
-                    else:
-                        PWMa.ChangeDutyCycle(lmotor)
-                        PWMb.ChangeDutyCycle(rmotor)
-            
-                sleep(1)
-        except KeyboardInterrupt:
-            print("Keyboard interrupt detected: stopping")
-            exit()
-        except IOError:
-            print("Controller not found")
-            sleep(1)
+    if lowspeedheld is not None: 
+        PWMa.ChangeDutyCycle(lmotor * LOWSPEED)
+        PWMb.ChangeDutyCycle(rmotor * LOWSPEED)
+    else:
+        PWMa.ChangeDutyCycle(lmotor)
+        PWMb.ChangeDutyCycle(rmotor)
 
-
-remotecontrol()
-
-
-
-
-        
-    
-
-
-    
+while True:
+    try:
+        with ControllerResource as joystick:
+            while joystick.connected:
+                remotecontrol()
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected: stopping")
+        exit()
+    except IOError:
+        print("Controller not found")
+        sleep(1)
