beb5a8dfded919422717426ed34ed9442c3eae22
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-11 10:52:01 +0100

    Update remotecontrol.py

--- remotecontrol.py
+++ remotecontrol.py
@@ -1,15 +1,15 @@
-from time import sleep
-import RPi.GPIO as GPIO # remember to install
-from approxeng.input.selectbinder import ControllerResource # remember to install
+from approxeng.input.selectbinder import ControllerResource
 from mcsmotors import *
 while True:
     try:
         with ControllerResource() as joystick:
             while joystick.connected:
+                print("joystick connected")
                 rvalue = joystick['ry']
                 lvalue = joystick['ly'] #joystick read values
                 lowspeedheld = joystick['r1']   #if r1 is not held, library returns value of None, so we need to check that it is not None
-                if lowspeedheld is not None: 
+                if lowspeedheld is not None:
+                    print("Lowspeed mode!") 
                     LOWSPEED = 0.4
                 else:
                     LOWSPEED = 0.0
