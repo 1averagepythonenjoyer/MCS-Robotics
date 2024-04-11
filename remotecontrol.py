f25b2266a1134f50b2cdaf8e3c201fd8ac122c96
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-11 14:07:28 +0100

    Update remotecontrol.py

--- remotecontrol.py
+++ remotecontrol.py
@@ -1,5 +1,6 @@
 from approxeng.input.selectbinder import ControllerResource
-from mcsmotors import *
+import mcsmotors 
+from time import sleep
 while True:
     try:
         with ControllerResource() as joystick:
@@ -13,12 +14,11 @@ while True:
                     LOWSPEED = 0.4
                 else:
                     LOWSPEED = 0.0
-                setmotor(rvalue, lvalue)
+                mcsmotors.setmotor(rvalue, lvalue)
                 print('Left: {}, Right: {}'.format(rvalue, lvalue))
-                print('Left duty: {}, Right duty: {}'.format(rmotor, lmotor))               
+                sleep(0.1)
     except KeyboardInterrupt:
         print("Keyboard interrupt detected: stopping")
         exit()
     except IOError:
         print("Controller not found")
-        sleep(1)
