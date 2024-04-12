83aca1dcaebad0067098d79caa69284979403f57
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-12 15:05:51 +0100

    Update remotecontrol.py

--- remotecontrol.py
+++ remotecontrol.py
@@ -1,24 +1,33 @@
 from approxeng.input.selectbinder import ControllerResource
-import mcsmotors 
+import mcsmotors
 from time import sleep
+import blinkylights
+blinkylights.blinkylights_on()
 while True:
     try:
         with ControllerResource() as joystick:
             while joystick.connected:
                 print("joystick connected")
-                rvalue = joystick['ry']
+                rvalue = joystick['rx']
                 lvalue = joystick['ly'] #joystick read values
-                lowspeedheld = joystick['r1']   #if r1 is not held, library returns value of None, so we need to check that it is not None
-                if lowspeedheld is not None:
-                    print("Lowspeed mode!") 
+                #lowspeedheld = joystick['r1']   #if r1 is not held, library returns value of None, so we need to check that it is not None
+                #if lowspeedheld is not None:
+
+                if joystick['r1']:
+                    print("Lowspeed mode!")
                     LOWSPEED = 0.4
                 else:
-                    LOWSPEED = 0.0
-                mcsmotors.setmotor(rvalue, lvalue)
-                print('Left: {}, Right: {}'.format(rvalue, lvalue))
-                sleep(0.1)
+                    LOWSPEED = 1.0
+                mcsmotors.yawthrottle(rvalue,lvalue, LOWSPEED)
+#                rvalue = rvalue *100
+#                lvalue = lvalue *100
+#                mcsmotors.setmotor(rvalue*LOWSPEED, lvalue*LOWSPEED)
+                #print('Left: {}, Right: {}'.format(lvalue, rvalue))
+                sleep(0.05)
     except KeyboardInterrupt:
         print("Keyboard interrupt detected: stopping")
+        blinkylights.blinkylights_off()
         exit()
     except IOError:
         print("Controller not found")
+        blinkylights.blinkylights_off()
