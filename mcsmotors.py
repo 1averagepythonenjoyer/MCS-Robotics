17cd1b93651347def7d1b5e3def0c9e5029e3ace
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-12 15:06:17 +0100

    Update mcsmotors.py

--- mcsmotors.py
+++ mcsmotors.py
@@ -1,9 +1,8 @@
 import RPi.GPIO as GPIO # remember to install
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-PWM_ENA = 13  
+PWM_ENA = 13
 GPIO.setup(PWM_ENA, GPIO.OUT)
 PWMa = GPIO.PWM(PWM_ENA, 100)
 PWMa.start(0)
@@ -13,7 +12,7 @@ GPIO.setup(PWM_ENB, GPIO.OUT)
 PWMb= GPIO.PWM(PWM_ENB, 100)
 PWMb.start(0)
 
-#h bridge logic 
+#h bridge logic
 #currently set to forward motion
 IN1= 19      #INPUT1 and INPUT2 logic pins on L298 control direction of left motor i believe
 GPIO.setup(IN1, GPIO.OUT) #double check which way round it is
@@ -29,12 +28,16 @@ GPIO.setup(IN4, GPIO.OUT)
 GPIO.output(IN4, GPIO.LOW)
 
 RMcorrection = 1.0 #to offset drift of motors (1 motor spinning faster than the other: left motor spins faster than the right I think)
-LMcorrection = 0.785
-LOWSPEED = 0.0
-def map_range(a, input_min, input_max, output_min, output_max): #nicky's function. Not entirely sure what it does. 
-    return (a - input_min) * (output_max - output_min) // (input_max - input_min) + output_min #a is placeholder value
+LMcorrection = 0.95
+def yawthrottle(yaw, throttle, speedfactor):   #yaw and throttle need to be floats from -1.0 to 1.0
+    left = (throttle + yaw)
+    right = (throttle - yaw)
+    scale = float(100) / max(1, abs(left), abs(right))
+    setmotor(right*scale, left*scale, speedfactor)
 
-def setmotor(right_power, left_power):
+def setmotor(right_power, left_power, speedfactor):
+    print("Right power:", right_power)
+
     #check h bridge
     if right_power < 0:
         GPIO.output(IN1, GPIO.LOW)
@@ -48,22 +51,16 @@ def setmotor(right_power, left_power):
     elif left_power >= 0:
         GPIO.output(IN3, GPIO.HIGH)
         GPIO.output(IN4, GPIO.LOW)
-    #rmotor = map_range(right_power,0,100,0,100) 
+    #rmotor = map_range(right_power,0,100,0,100)
     #lmotor = map_range(left_power,0,100,0,100)
      #values to be sent to motors
-    #send values to motor controller 
+    #send values to motor controller
+    #print("Right power:", right_power)
+    #print("Left power:", left_power)
+    #right_power = abs(right_power)
     if -100 <= right_power <= 100:
-        PWMa.ChangeDutyCycle(abs(right_power) * RMcorrection * LOWSPEED) #abs() always returns positive so changedutycycle doesnt give an error. 
+        PWMa.ChangeDutyCycle(int(abs(right_power) * RMcorrection*speedfactor)) #abs() always returns positive so changedutycycle doesnt give an error.
     if -100 <= left_power <= 100:
-        PWMb.ChangeDutyCycle(abs(left_power) * LMcorrection * LOWSPEED)
-    print("Right:", abs(right_power) * RMcorrection * LOWSPEED)
-    print("Left: ", abs(left_power) * LMcorrection * LOWSPEED)
-    
-    
-
-    
-    
-    
-
-    
-    
+        PWMb.ChangeDutyCycle(int(abs(left_power) * LMcorrection*speedfactor))
+    print("Right Duty:", abs(right_power) * RMcorrection)
+    print("Left Duty: ", abs(left_power) * LMcorrection)
