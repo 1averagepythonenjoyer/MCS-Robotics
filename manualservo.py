02629bb995953ea54922a7f04a108e55aaca849f
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-04-16 14:11:17 +0100

    Update manualservo.py

--- manualservo.py
+++ manualservo.py
@@ -15,19 +15,21 @@ else:
 vservo = Servo(12, min_value=0, max_value=180, min_pulse=1.0, max_pulse= 2.0, frequency=50) #GPIO BCM numbers. pin 32
 hservo = Servo(13, min_value=0, max_value=180, min_pulse=1.0, max_pulse= 2.0, frequency=50) # horizontal servo pin 33
 # library auto checks if values are in range here
+vservo.start()
+hservo.start()
 global servospeedfactor
-servospeedfactor = 1.0
+servospeedfactor = 2.0
 vservo_max = 180   #change these after tuning
 vservo_min = 0    # change these after tuning
-vservo_step = 0.1
-vservo_current = 90
+vservo_step = 0.5
+vservo_current = 70
 
 hservo_max = 180  #change these after tuning
 hservo_min = 0 #change these after tuning
-hservo_step = 0.1
-hservo_current = 90
+hservo_step = 0.5
+hservo_current = 70
 
-delay = float(0.05)
+delay = float(0.10)
 
 vservo.write(90)  
 hservo.write(90)
@@ -41,6 +43,8 @@ while True:
                 slow = joystick['l1']   #turns more slowly if l1 trigger is held
                 if slow is not None:
                     servospeedfactor = 0.3
+                else: 
+                    servospeedfactor = 1.0
                 
                 moveup = joystick['dup']  #dpad up button
                 movedown = joystick['ddown']  #dpad down button
@@ -73,14 +77,16 @@ while True:
                 hservo.write(hservo_current)
                 
                 vturnrate = float(vservo_step/delay)     #DEBUG: prints turn rate of the servo
-                print("vertical turn rate:", vturnrate, "degrees per second"
+                print("vertical turn rate:", vturnrate, "degrees per second")
                 hturnrate = float(hservo_step/delay)
                 print("horizontal turn rate:", hturnrate, "degrees per second")
-                
+                print("horizontal:", hservo_current)
+                print("vertical;", vservo_current)
                 time.sleep(delay)
     except KeyboardInterrupt:
         print("Keyboard interrupt detected: stopping")
-
+        vservo.stop()
+        hservo.stop()
         exit()
     except IOError:
         print("Controller not found")
