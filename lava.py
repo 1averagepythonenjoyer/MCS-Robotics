b3b2d13afeefa0b4f9f76b47e360242145ceba6d
18lliu <152213070+18lliu@users.noreply.github.com>
2024-04-17 13:53:40 +0100

    Update lava.py

--- lava.py
+++ lava.py
@@ -1,36 +1,35 @@
 try:   #Right now, the deal is is that the motors are to slow moving to do +amount and -amount. Later just remove the -amount 
-    import mcsmotors
+    import RPi.GPIO as GPIO
+    from mcsmotors import *
 
-    mcsmotors.RPi.GPIO.setmode(GPIO.BOARD)
-    mcsmotors.RPi.GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
 
     left_sensor_pin = 11
     middle_sensor_pin = 10
     right_sensor_pin = 12
 
-    mcsmotors.RPi.GPIO.setup(left_sensor_pin, GPIO.IN)
-    mcsmotors.RPi.GPIO.setup(middle_sensor_pin, GPIO.IN)
-    mcsmotors.RPi.GPIO.setup(right_sensor_pin, GPIO.IN)
+    GPIO.setup(left_sensor_pin, GPIO.IN)
+    GPIO.setup(middle_sensor_pin, GPIO.IN)
+    GPIO.setup(right_sensor_pin, GPIO.IN)
     
-    speed_factor = 1.0#Please enter a number from 0 to 1 inclusive
+    speed_factor = 1#Please enter a number from 0 to 1 inclusive
     if speed_factor < 0 or speed_factor > 1:
         print("Please give a speed factor of between 0 and 1")
         quit()
 
-    def forward(yaw, throttle):
+    def forward(yaw, throttle, speed_factor):
         try:
-            mcsmotors.yawthrottle(yaw, throttle, speedfactor)
+            yawthrottle(yaw, throttle, speed_factor)
         except KeyboardInterrupt:
             print('manual stop while moving')    
             quit()
         
     def check():
-        global left_sensor,
-        global middle_sensor
-        global right_sensor
-        left_sensor = mcsmotors.RPi.GPIO.input(left_sensor_pin)
-        middle_sensor = mcsmotors.RPi.GPIO.input(middle_sensor_pin)
-        right_sensor = mcsmotors.RPi.GPIO.input(right_sensor_pin)
+        global left_sensor, middle_sensor, right_sensor
+        left_sensor = GPIO.input(left_sensor_pin)
+        middle_sensor = GPIO.input(middle_sensor_pin)
+        right_sensor = GPIO.input(right_sensor_pin)
 
     while True:
         try:   
@@ -39,35 +38,35 @@ try:   #Right now, the deal is is that the motors are to slow moving to do +amou
             check()
                 
             if left_sensor == 0 and right_sensor == 0 and middle_sensor ==1:
-                forward(0, 0.3)
+                forward(0, 0.3, speed_factor)
                     
             elif left_sensor == 1 and middle_sensor == 1 and right_sensor == 0:
-                forward(-0.15, 0.3)
+                forward(-0.15, 0.3, speed_factor)
                 
             elif right_sensor == 1 and middle_sensor == 1 and left_sensor == 0:
-                forward(0.15, 0.3)
+                forward(0.15, 0.3, speed_factor)
 
             else:
                 if middle_sensor == 0 and left_sensor == 0 and right_sensor == 1:
-                    forward(0.35, 0.3)
+                    forward(0.35, 0.3, speed_factor)
                     check()
                     while middle_sensor == 0 and left_sensor == 0 and right_sensor == 0:
-                        forward(0.7+amount, 0.3)
+                        forward(0.7+amount, 0.3, speed_factor)
                         amount+=2
                         check()
                                 
                 elif middle_sensor == 0 and right_sensor== 0 and left_sensor == 1:
-                    forward(-0.35, 0.3)
+                    forward(-0.35, 0.3, speed_factor)
                     check()
                     
                     while middle_sensor == 0 and right_sensor== 0 and left_sensor == 0:
-                        forward(-0.7-amount, 0.3)
+                        forward(-0.7-amount, 0.3, speed_factor)
                         amount+=2
                         check()
                         
                 else: #no sensors
                     if left_sensor == 0 and right_sensor == 0 and middle_sensor == 0:
-                        forward(0, 0.3)
+                        forward(0, 0.3, speed_factor)
                         
                                 
         except KeyboardInterrupt:
