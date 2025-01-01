4b1865366721d53b2be5a610acea174e5154e8ef
b1mgus <80120901+Lumpy20197@users.noreply.github.com>
2025-01-01 17:34:53 +0000

    main.py
    As of now 1st January: cooked 👎

--- ROBOCON/main.py
+++ ROBOCON/main.py
@@ -1,1 +1,33 @@
+import robot
+from time import sleep
 
+r = robot.Robot()
+
+t =   #Turn rate (degrees per second at full speed)
+v =   #speed (centimeters per second at 90% speed)
+
+
+def twist(angle): 
+    
+    turnT = abs(angle) / t  # Calculate time needed to turn
+
+    #ADJUST THESE VALUES BELOW DEPENDING ON ACCURACY OF TURN
+    if angle < 0:  # Turn left
+        r.motors[0] = -15  # Left motor
+        r.motors[1] = 15  # Right motor
+    else:  # Turn right
+        r.motors[0] = 15  # Left motor
+        r.motors[1] = -15  # Right motor
+
+    sleep(turnT)  
+    r.motors[0] = 0  
+    r.motors[1] = 0
+
+def move(distance):
+    
+    moveT = distance / v  # Calculate time needed to move
+    r.motors[0] = 90 
+    r.motors[1] = 90
+    sleep(moveT) 
+    r.motors[0] = 0  
+    r.motors[1] = 0
