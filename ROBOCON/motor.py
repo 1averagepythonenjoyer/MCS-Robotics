192fcdcd923a47bb95e73f9dfc46192c87a825d9
Lintian <lintian.hk.sz@gmail.com>
2025-01-20 14:19:36 +0000

    Update motor.py
    UNDER CONSTRUCTION PLEASE DO NOT TOUCH I have to go cuz lunch is finished anyways Mr Jaworski told me to do this stuff basically turning but with the box (for the last box) and also to collect the box

--- ROBOCON/motor.py
+++ ROBOCON/motor.py
@@ -4,12 +4,25 @@ from time import sleep
 r = robot.Robot()
 
 t = 127   #Turn rate (degrees per second at full speed)  find this later 
+twist =  #Twist rate - speed robot turns 90 degrees with one motor stationary - left and right motor different so robot speed different
 v = 0.321  #speed (centimeters per second)
 
+def twist(movement): #Objective of 'twist' is for movement of robot to collect box and also move with the box becuz if robot turns to sharply the box is gonna not be with the robot 
+   
+    if movement == 'left' or 'box': 
+        r.motors[0] = 0 #left motor stationary
+        r.motors[1] = 100 #right motor - change if necassary
+        sleep(twist)
+        r.motors[1] = 0
+    elif movement == 'right':
+        pass
+    else:
+        print('sorry, your commands did not match with our options, please try again or seek help')
+        return
 
-def twist(angle): 
+def turn(angle): 
     
-    twistT = (abs(angle) / t) + 0.05 # Calculate time needed to twist
+    turnT = (abs(angle) / t) + 0.05 # Calculate time needed to turn
 
     while angle > 360:
         angle -= 360
@@ -22,7 +35,7 @@ def twist(angle):
         r.motors[0] =  100 # Left motor
         r.motors[1] = -100  # Right motor
 
-    sleep(twistT)  
+    sleep(turnT)  
     r.motors[0] = 0  
     r.motors[1] = 0
 
