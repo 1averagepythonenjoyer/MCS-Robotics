93ccc144cc095c17d21c697691d504842a939aa5
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2025-03-18 22:20:24 +0000

    Update motor.py

--- ROBOCON/motor.py
+++ ROBOCON/motor.py
@@ -1,7 +1,3 @@
-import robot
-from time import sleep
-r = robot.Robot(max_motor_voltage=12)
-
 Lm_multi = 0.956
 Rm_multi = 1
 Velocity = 0.97
@@ -22,35 +18,38 @@ def spin(angle):
     spinT = (abs(angle)/t) + 0.05
     while angle > 360:
         angle -= 360
-        if angle < 0:
-            r.motors[0] = 100*Lm_multi*reductionInSpeed
-            r.motors[1] = 100*Rm_multi*reductionInSpeed
-        else:
-            r.motors[0] = -100*Lm_multi*reductionInSpeed
-            r.motors[1] = 100*Rm_multi*reductionInSpeed
+    if angle < 0:
+        r.motors[0] = 100*Lm_multi*reductionInSpeed
+        r.motors[1] = 100*Rm_multi*reductionInSpeed
+    else:
+        r.motors[0] = -100*Lm_multi*reductionInSpeed
+        r.motors[1] = 100*Rm_multi*reductionInSpeed
 
-        sleep(spinT)
+    sleep(spinT)
+    r.motors[0] = 0
+    r.motors[1] = 0
+
+TwistRateRight = 142
+TwistRateLeft = 147
+twist_multi = 0.5
+
+
+def twist(angle): #twist 90 degrees to grab the box. 
+    #positive angle: turn right
+    #negative angle: turn left
+    if angle < 0: 
+        twistTRight = abs((angle/TwistRateRight)/twist_multi)
+        r.motors[0] = 0
+        r.motors[1] = 100*Rm_multi*twist_multi
+        sleep(twistTRight)
         r.motors[0] = 0
         r.motors[1] = 0
-
-TwistRateRight90 = 147
-
-def Right_twist90(): #twist 90 degrees to grab the box. 
-    twist90T = 90/TwistRateRight90
-    r.motors[0] = 0
-    r.motors[1] = 100*Rm_multi
-    sleep(twist90T)
-    r.motors[0] = 0
-    r.motors[1] = 0
-
-TwistRateRight270 = 162
-
-def Right_twist270():  #twist 270 to go back to starting orientation before picking up the box. We pick up the box, store, then move back
-    twist270T = 270/TwistRateRight270
-    r.motors[0] = 0
-    r.motors[1] = 100*Rm_multi
-    sleep(twist270T)
-    r.motors[0] = 0
-    r.motors[1] = 0
-
-#DOES NOT INCLUDE THE POSITION UPDATE FUNCTION: THIS WILL BE PLACED IN MAIN, BUT REMINDER TO ADD IF TESTING FROM THIS FILE.##!!!!!!
+    elif angle > 0: 
+        twistTLeft = abs((angle/TwistRateLeft)/twist_multi)
+        r.motors[0] = 100*Lm_multi*twist_multi  #need to tune for twisting left
+        r.motors[1] = 0
+        sleep(twistTLeft)
+        r.motors[0] = 0
+        r.motors[1] = 0
+    else:
+        pass
