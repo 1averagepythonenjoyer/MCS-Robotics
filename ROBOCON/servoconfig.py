d78d85da99ca24bd7439b9c4fab1b77799f852aa
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2025-03-18 14:01:04 +0000

    Update servoconfig.py

--- ROBOCON/servoconfig.py
+++ ROBOCON/servoconfig.py
@@ -1,12 +1,41 @@
 import smbus2
+import robot
+
+from time import sleep
 
 bus = smbus2.SMBus(1)
 r = robot.Robot() #dont need in main
 
-def set_servos_to_angle(angle):
- #angle range = 1700 , 4600, 7500
- #mid value 70
- r.servos[0].mode = robot.PWM_SERVO 
- value = 1700 + int(angle/180*5800)
- bus.write_byte_data(0x08, 1, value >> 8)
- bus.write_byte_data(0x08, 2, value & 0xff)
+r.servos[0].mode = robot.PWM_SERVO 
+r.servos[1].mode = robot.PWM_SERVO 
+
+def set_arm_servo_to_angle(angle):
+#angle range = 1700 , 4600, 7500
+#mid value 70
+    value = 1700 + int(angle/180*5800)
+    bus.write_byte_data(0x08, 1, value >> 8)
+    bus.write_byte_data(0x08, 2, value & 0xff)
+
+def set_claw_servo_to_angle(angle):
+#angle range = 1700 , 4600, 7500
+#mid value 70 
+    value = 1700 + int(angle/180*5800)
+    bus.write_byte_data(0x08, 3, value >> 8)
+    bus.write_byte_data(0x08, 4, value & 0xff)
+
+
+
+set_arm_servo_to_angle(3)
+sleep(2)
+set_claw_servo_to_angle(10)
+sleep(2)
+set_claw_servo_to_angle(160)
+sleep(2)
+set_arm_servo_to_angle(170)
+sleep(2)
+set_claw_servo_to_angle(10)
+sleep(2)
+set_arm_servo_to_angle(0)
+# set_arm_servo_to_angle(180)
+# sleep(2)
+# set_claw_servo_to_angle(0)
