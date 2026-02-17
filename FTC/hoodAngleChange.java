d15faa546eddf56c52618ca8ccc27926c52732ba
clavicularcd <143657738+clavicularcd@users.noreply.github.com>
2026-02-17 20:58:25 +0000

    Update hoodAngleChange.java
    moved begintime=getruntime to inside the loop so that it updates when the button is actually pressed.
    
    added &&opModeisActive so that stopping the program on the driver station also stops the motors. otherwise motors will keep spinning until the timer runs out.

--- FTC/hoodAngleChange.java
+++ FTC/hoodAngleChange.java
@@ -1,92 +1,90 @@
 package org.firstinspires.ftc.teamcode;
 
-
-
-
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
-
-
-
-
-@TeleOp(name = "hoodAngleChangeTest")
+@TeleOp(name = "Hood Angle Change (While Loop Version)")
 public class hoodAngleChange extends LinearOpMode {
+
     private CRServo s1;
-
     private DigitalChannel hoodSwitch;
+    // private DcMotor flywheel;
 
-    private DcMotor flywheel;
-
-
-
-
-
+    // function to check switch to return bool
     public boolean switchStatus() {
-        telemetry.addData("Switch State", !hoodSwitch.getState());
         return !hoodSwitch.getState();
     }
 
-
-
     @Override
     public void runOpMode() {
+
         s1 = hardwareMap.get(CRServo.class, "s1AsServo");
-        flywheel = hardwareMap.get(DcMotor.class, "flywheel");
+        // flywheel = hardwareMap.get(DcMotor.class, "flywheel");
 
         hoodSwitch = hardwareMap.get(DigitalChannel.class, "hoodSwitch");
         hoodSwitch.setMode(DigitalChannel.Mode.INPUT);
 
-        double BEGINTIME = getRuntime();
-        // fill in after calibration
-        double TIME_FACTOR = 0.002;
-        // input of function
-        double ANGLE_TO_ROTATE = -1;
-
-
-        boolean isButtonPressed = true;
-
-//        double TURNSTARTTIME = getRuntime();
+        final double TIME_FACTOR = 0.002;
+        double ANGLE_TO_ROTATE = 10;
+        final double TIME_TAKEN_TO_SHOOT = 5.0;
 
         s1.setDirection(CRServo.Direction.REVERSE);
 
+        telemetry.addLine("Initialized. Waiting for start...");
+        telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
 
+            // If X is pressed move the servo
             if (gamepad2.x) {
-                ANGLE_TO_ROTATE = 10;
 
+                // move hood
+                double beginTime = getRuntime(); //record time when X is pressed
 
-                while (getRuntime() < (TIME_FACTOR * ANGLE_TO_ROTATE) + BEGINTIME) {
-                    s1.setPower(0.8);
+                // Loop until the calculated time is reached
+                while (opModeIsActive() && getRuntime() < beginTime + (TIME_FACTOR * ANGLE_TO_ROTATE)) {
+                    s1.setPower(-0.8);
 
-                    telemetry.addData("angle:", ANGLE_TO_ROTATE);
-                    telemetry.addData("time:", TIME_FACTOR * ANGLE_TO_ROTATE);
+                    telemetry.addData("Status", "Moving Hood");
+                    telemetry.addData("Target Time", TIME_FACTOR * ANGLE_TO_ROTATE);
                     telemetry.update();
                 }
 
-                s1.setPower(0);  //stop servo
-
-                double POWER_START_TIME = getRuntime();
-                double TIME_TAKEN_TO_SHOOT = 5;
-
-                while (getRuntime() < POWER_START_TIME + TIME_TAKEN_TO_SHOOT) {
-                    flywheel.setPower(0.8);
+//                s1.setPower(0); // Stop servo
+//
+//                // --- 2. SPIN THE FLYWHEEL ---
+//                double powerStartTime = getRuntime(); // Reset the timer for the flywheel
+//
+//                while (opModeIsActive() && getRuntime() < powerStartTime + TIME_TAKEN_TO_SHOOT) {
+//                    flywheel.setPower(0.8);
+//
+//                    telemetry.addData("Status", "Shooting!");
+//                    telemetry.update();
+//                }
+//
+//                flywheel.setPower(0); // Stop flywheel
+            }
+            // If X is NOT being pressed, recalibrate
+            else {
+                // If the switch is not pressed, move backwards to find home
+                if (!switchStatus()) {
+                    s1.setPower(0.5);
+                    telemetry.addData("Status", "Homing (Finding Zero)");
                 }
-                flywheel.setPower(0);
+                // If the switch is pressed, stop
+                else {
+                    s1.setPower(0);
+                    telemetry.addData("Status", "Ready to Shoot");
+                }
             }
 
-                //RESET SERVO POSITION
-            if (!switchStatus()) {
-                s1.setPower(-0.5);
-            } else {
-                s1.setPower(0);  //stop servo
-            }
+            telemetry.addData("Switch Pressed", switchStatus());
+            telemetry.update();
         }
     }
 }
