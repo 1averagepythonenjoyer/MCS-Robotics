d018703d4ef1c9aa26fd90429dae8c096a3dc390
Cameraboyy <165475130+Cameraboyy@users.noreply.github.com>
2026-02-21 14:30:28 +0000

    Refactor storageKickers for improved readability

--- FTC/storage/storageKickers.java
+++ FTC/storage/storageKickers.java
@@ -1,3 +1,4 @@
+
 // For my darling dearest precious cherished favourite Calvin -
 // This very simple program uses buttons a, b and x to control 'kick' each servo 90 degrees, and uses button y to trigger all 3 'kicks' in sequence
 // There are three (double type) tunable variables:
@@ -6,6 +7,7 @@
 // DELAY_BETWEEN_KICKS = 1;               number of seconds allotted for the servo that is up, to move down to resting position before the next servo can 'kick' (to prevent collisions between 'kicks')
 // P.S. Also this is kinda random, but could you send me the photos you took of the bitcoin thing with the gyro     (^ω^)
 
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -13,12 +15,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
 @TeleOp(name = "storageKicker")
 public class storageKickers extends LinearOpMode {
 
     private Servo storageServo1;
+
     private Servo storageServo2;
     private Servo storageServo3;
+
+
+
 
     enum SequenceState {
         IDLE,
@@ -31,118 +38,157 @@ public class storageKickers extends LinearOpMode {
         SERVO1_WAIT_DOWN,
         SERVO2_WAIT_DOWN,
         SERVO3_WAIT_DOWN
+
+
     }
 
     SequenceState currentState = SequenceState.IDLE;
 
+
+
     @Override
     public void runOpMode() {
+
 
         storageServo1 = hardwareMap.get(Servo.class, "storageServo1");
         storageServo2 = hardwareMap.get(Servo.class, "storageServo2");
         storageServo3 = hardwareMap.get(Servo.class, "storageServo3");
 
-        double startingServoPosition = 0;
-        double DELAY_KICKER_STAYING_UP_TIME = 0.3;
-        double DELAY_BETWEEN_KICKS = 1;
-
-        boolean sequenceActive = false;
-
         waitForStart();
 
-        ElapsedTime stopwatch = new ElapsedTime();
 
-        // Set servos to start positions
-        storageServo1.setPosition(startingServoPosition);
-        storageServo2.setPosition(startingServoPosition);
-        storageServo3.setPosition(startingServoPosition);
 
-        while (opModeIsActive()) {
 
-            switch (currentState) {
 
-                case IDLE:
-                    if (gamepad1.a) {
-                        currentState = SequenceState.SERVO1_UP;
-                    } else if (gamepad1.b) {
-                        currentState = SequenceState.SERVO2_UP;
-                    } else if (gamepad1.x) {
-                        currentState = SequenceState.SERVO3_UP;
-                    } else if (gamepad1.y) {
-                        currentState = SequenceState.SERVO1_UP;
-                        sequenceActive = true;
-                    }
-                    break;
+            double startingServoPosition = 0;
+            double DELAY_KICKER_STAYING_UP_TIME = 0.3;
+            double DELAY_BETWEEN_KICKS = 1;
 
-                case SERVO1_UP:
-                    storageServo1.setPosition(startingServoPosition + 0.3);
-                    stopwatch.reset();
-                    currentState = SequenceState.SERVO1_WAIT_UP;
-                    break;
+            boolean sequenceActive = false;
 
-                case SERVO1_WAIT_UP:
-                    if (stopwatch.seconds() >= DELAY_KICKER_STAYING_UP_TIME) {
-                        storageServo1.setPosition(startingServoPosition);
-                        currentState = SequenceState.SERVO1_WAIT_DOWN;
-                        stopwatch.reset();
-                    }
-                    break;
 
-                case SERVO1_WAIT_DOWN:
-                    if (stopwatch.seconds() >= DELAY_BETWEEN_KICKS) {
-                        if (!sequenceActive) {
-                            currentState = SequenceState.IDLE;
-                        } else {
+            //    set servos to start positions
+            storageServo1.setPosition(startingServoPosition);
+            storageServo2.setPosition(startingServoPosition);
+            storageServo3.setPosition(startingServoPosition);
+
+            ElapsedTime stopwatch = new ElapsedTime();       //Important set stopwatch
+
+
+            waitForStart();
+
+
+            while (opModeIsActive()) {
+
+
+
+
+                switch (currentState) {
+                    case IDLE:           // Check for Controller Inputs
+                        if (gamepad1.a) {                           //cross
+                            currentState = SequenceState.SERVO1_UP;
+                        } else if (gamepad1.b) {                    //circle
                             currentState = SequenceState.SERVO2_UP;
-                        }
-                    }
-                    break;
-
-                case SERVO2_UP:
-                    storageServo2.setPosition(startingServoPosition + 0.3);
-                    stopwatch.reset();
-                    currentState = SequenceState.SERVO2_WAIT_UP;
-                    break;
-
-                case SERVO2_WAIT_UP:
-                    if (stopwatch.seconds() >= DELAY_KICKER_STAYING_UP_TIME) {
-                        storageServo2.setPosition(startingServoPosition);
-                        currentState = SequenceState.SERVO2_WAIT_DOWN;
-                        stopwatch.reset();
-                    }
-                    break;
-
-                case SERVO2_WAIT_DOWN:
-                    if (stopwatch.seconds() >= DELAY_BETWEEN_KICKS) {
-                        if (!sequenceActive) {
-                            currentState = SequenceState.IDLE;
-                        } else {
+                        } else if (gamepad1.x) {                    //square
                             currentState = SequenceState.SERVO3_UP;
+                        } else if (gamepad1.y) {                    //triangle
+                            currentState = SequenceState.SERVO1_UP;
+                            sequenceActive = true;
                         }
-                    }
-                    break;
+                        break;
 
-                case SERVO3_UP:
-                    storageServo3.setPosition(startingServoPosition + 0.3);
-                    stopwatch.reset();
-                    currentState = SequenceState.SERVO3_WAIT_UP;
-                    break;
-
-                case SERVO3_WAIT_UP:
-                    if (stopwatch.seconds() >= DELAY_KICKER_STAYING_UP_TIME) {
-                        storageServo3.setPosition(startingServoPosition);
-                        currentState = SequenceState.SERVO3_WAIT_DOWN;
+                    case SERVO1_UP:
+                        storageServo1.setPosition(startingServoPosition + 0.3);  // extend kicker - to push ball up
                         stopwatch.reset();
-                    }
-                    break;
+                        currentState = SequenceState.SERVO1_WAIT_UP;
+                        telemetry.addLine("Servo kicking up");
 
-                case SERVO3_WAIT_DOWN:
-                    if (stopwatch.seconds() >= DELAY_BETWEEN_KICKS) {
-                        currentState = SequenceState.IDLE;
-                        sequenceActive = false;
-                    }
-                    break;
+                        break;
+
+                    case SERVO1_WAIT_UP:
+                        if (stopwatch.seconds() >= DELAY_KICKER_STAYING_UP_TIME) {  // Stay up for some period of time
+                            storageServo1.setPosition(startingServoPosition);   //retract kicker
+                            currentState = SequenceState.SERVO1_WAIT_DOWN;
+                            telemetry.addLine("Servo going down");
+
+                            stopwatch.reset();
+                        }
+
+                        break;
+
+                    case SERVO1_WAIT_DOWN:
+                        if (stopwatch.seconds() >= DELAY_BETWEEN_KICKS) {       // going down for some period of time before next servo is launched
+                            if (!sequenceActive) {
+                                currentState = SequenceState.IDLE;
+                            } else {
+                                currentState = SequenceState.SERVO2_UP;
+                            }
+                        }
+                        break;
+
+
+                    case SERVO2_UP:
+                        storageServo2.setPosition(startingServoPosition + 0.3);
+                        stopwatch.reset();
+                        currentState = SequenceState.SERVO2_WAIT_UP;
+                        telemetry.addLine("Servo kicking up");
+
+                        break;
+
+                    case SERVO2_WAIT_UP:
+                        if (stopwatch.seconds() >= DELAY_KICKER_STAYING_UP_TIME) {
+                            storageServo2.setPosition(startingServoPosition);
+                            currentState = SequenceState.SERVO2_WAIT_DOWN;
+                            telemetry.addLine("Servo going down");
+
+                            stopwatch.reset();
+                        }
+
+                        break;
+
+                    case SERVO2_WAIT_DOWN:
+                        if (stopwatch.seconds() >= DELAY_BETWEEN_KICKS) { // going down for some period of time before next servo is launched
+                            if (!sequenceActive) {
+                                currentState = SequenceState.IDLE;
+                            } else {
+                                currentState = SequenceState.SERVO3_UP;
+                            }
+                        }
+                        break;
+
+
+                    case SERVO3_UP:
+                        storageServo3.setPosition(startingServoPosition + 0.3);
+                        telemetry.addLine("Servo kicking up");
+
+                        stopwatch.reset();
+                        currentState = SequenceState.SERVO3_WAIT_UP;
+                        break;
+
+                    case SERVO3_WAIT_UP:
+                        if (stopwatch.seconds() >= DELAY_KICKER_STAYING_UP_TIME) {
+                            storageServo3.setPosition(startingServoPosition);
+                            currentState = SequenceState.SERVO3_WAIT_DOWN;
+                            stopwatch.reset();
+                            telemetry.addLine("Servo going down");
+
+                        }
+
+                        break;
+
+                    case SERVO3_WAIT_DOWN:
+                        if (stopwatch.seconds() >= DELAY_BETWEEN_KICKS) { // going down for some period of time before next servo is launched
+
+                            currentState = SequenceState.IDLE;   // either way this is the end of the sequence so must be idle
+                            sequenceActive = false;
+                        }
+                        break;
+
+
+                }
+
+
             }
         }
-    }
+    
 }
